23f75d693d7133d4d1c31c4d0b5bd01da9062b1d
Denys <derdek02@gmail.com>
2021-12-04 17:33:52 +0200

    add sorting logic

--- resources/views/programs/dashboard.blade.php
+++ resources/views/programs/dashboard.blade.php
@@ -16,6 +16,14 @@
         </div>
     </x-slot>
 
+    @php
+        $isEmpty = empty($sortType) || empty($sortColumn);
+        $isSortByPrograms = $isEmpty || ($sortType == 'desc' && $sortColumn == 'programs') || $sortColumn != 'programs';
+        $isSortByCategories = $isEmpty || ($sortType == 'desc' && $sortColumn == 'categories') || $sortColumn != 'categories';
+        $isSortByTypes = $isEmpty || ($sortType == 'desc' && $sortColumn == 'types') || $sortColumn != 'types';
+        $isSortByRating = $isEmpty || ($sortType == 'asc' && $sortColumn == 'rating') || $sortColumn != 'rating';
+    @endphp
+
     <div class="flex flex-col">
         <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
@@ -24,16 +32,16 @@
                         <thead class="bg-gray-50">
                             <tr>
                                 <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    Program name
+                                    <a href="{{ route('sortedPrograms', 'programs-'.($isSortByPrograms ? 'asc' : 'desc')) }}">Program name</a>
                                 </th>
                                 <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    Category
+                                    <a href="{{ route('sortedPrograms', 'categories-'.($isSortByCategories ? 'asc' : 'desc')) }}">Category</a>
                                 </th>
                                 <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    Type
+                                    <a href="{{ route('sortedPrograms', 'types-'.($isSortByTypes ? 'asc' : 'desc')) }}">Type</a>
                                 </th>
                                 <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    Rating
+                                    <a href="{{ route('sortedPrograms', 'rating-'.($isSortByRating ? 'desc' : 'asc')) }}">Rating</a>
                                 </th>
                                 @auth
                                     @can('edit programs')
