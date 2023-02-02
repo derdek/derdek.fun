a8e9063df25cf7ebcd592ee86c4a8cbe1aeaa2b4
Denys <derdek02@gmail.com>
2023-02-02 23:55:38 +0200

    add alt for logo

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -26,7 +26,7 @@
                 <div class="flex items-center justify-between w-full md:w-auto">
                   <a href="#">
                     <span class="sr-only">Workflow</span>
-                    <img class="h-8 w-auto sm:h-10" src="{{ asset('DerDek.png') }}">
+                    <img class="h-8 w-auto sm:h-10" src="{{ asset('DerDek.png') }}" alt="DerDek logo">
                   </a>
                   <div class="-mr-2 flex items-center md:hidden">
                     <button @click="mobile_menu = true" type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false">
@@ -71,7 +71,7 @@
             <div class="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
               <div class="px-5 pt-4 flex items-center justify-between">
                 <div>
-                  <img class="h-8 w-auto" src="{{ asset('DerDek.png') }}" alt="">
+                  <img class="h-8 w-auto" src="{{ asset('DerDek.png') }}" alt="DerDek logo">
                 </div>
                 <div class="-mr-2">
                   <button @click="mobile_menu = false" type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
